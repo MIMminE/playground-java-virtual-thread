c91c17f59689f1f5ebd50d47532304763731def2
MIMminE <weluv.ww@gmail.com>
2025-05-16 15:18:12 +0900

    project init

---
+++ repo/virtual_thread/src/test/java/nuts/playgroud/virtual_thread/VirtualThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package nuts.playgroud.virtual_thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualThreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
